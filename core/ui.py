b46f6889a3e6dacafd065a8db260a92bf57cc3e5
Tech0ne <clement.piasco@epitech.eu>
2023-07-02 02:35:16 +0200

    [ADD] Started to implement UI, using flask

--- core/ui.py
+++ core/ui.py
@@ -1,3 +1,50 @@
-from flask import Flask
+from flask import Flask, request
+from os.path import isfile
 
 from .log import *
+
+STILL_RUNNING = True
+
+webapp = Flask(__name__)
+
+def readfile(path):
+    if not isfile(path):
+        return readfile("static/html/error.html").replace("ERROR", "The requested ressource does not exist !")
+    with open(path, 'r') as f:
+        return f.read()
+
+@webapp.errorhandler(500)
+def internal_error(error):
+    return readfile("static/html/error.html").replace("ERROR", str(error))
+
+@webapp.route("/")
+def webhome():
+    return readfile("static/html/main.html")
+
+@webapp.route("/static/css/<path:name>")
+def get_some_css(name):
+    path = "static/css/" + name.replace("..", '')
+    return readfile(path)
+
+@webapp.route("/static/html/<path:name>")
+def get_some_css(name):
+    path = "static/html/" + name.replace("..", '')
+    return readfile(path)
+
+@webapp.route("/static/js/<path:name>")
+def get_some_css(name):
+    path = "static/js/" + name.replace("..", '')
+    return readfile(path)
+
+
+@webapp.route("/stopme")
+def tryandstop():
+    raise RuntimeError("SERVER_SHUTDOWN")
+
+@webapp.teardown_request
+def teardown(_):
+    if not STILL_RUNNING:
+        sys.exit(0)
+
+def webapp_run():
+    webapp.run(host="127.0.0.1", port=3456, debug=False)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from core import *
 
 # excel_file = read_excel_file("/home/clementp/Téléchargements/classes.xlsx")
-student = Student("Dupont", "Jean", "hello", "Cateperdrix", 1, 3, 2, 1, 0, 0, "Espagnole", "", "", [], [], "")
-print(student.name)
+student = Student("Dupont", "Jean", "g", "Cateperdrix", 1, 3, 2, 1, 0, 0, "Espagnole", "", "", [], [], "")
+
+webapp_run()
