9f104c7582cf94456ccc89ae97ddcf47d3db8ffa
siderations <138388000+siderations@users.noreply.github.com>
2023-07-02 18:21:41 +0200

    Add files via upload

---
+++ consignes.py
@@ -0,0 +1,14 @@
+"""
+                       Consignes :
+
+Consitituer 12 classes qui suivent une structure imposée, avec entre 30 et 35
+élèves par classe, par rapport à des critères (à respecter le mieux possible).
+
+Les critères :
+ 1- "à séparer de" et "à mettre avec"
+ 2- niveau scolaire
+ 3- comportement des élèves
+ 4- collège d'origine
+
+
+"""
